71bf86e146cbf17e71d0811d536fa9e12d5c956e
manikandan1985 <shanmani.ac@gmail.com>
2017-06-17 11:03:08 +0530

    first commit

---
+++ src/test/java/GoogleTest1/GoogleTest1.java
@@ -0,0 +1,26 @@
+package GoogleTest1;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTest1 {
+	@Test
+	public void logintest() throws IOException
+	{
+		
+		WebDriver wd=new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.co.in");
+		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C:\\workspace\\Mars\\GoogleTest1\\screenshot\\tesst.jpg"));
+		wd.quit();
+	}
+
+}
